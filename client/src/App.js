6faa74beab89bbc693e082f2bd7e7fd99b17261d
Mike2217 <msoles2217@gmail.com>
2021-12-08 13:06:47 -0500

    3 hours no progress

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,46 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { Link, Routes, Route } from 'react-router-dom'
 import { grabUserData, config, BASE_URL } from './services'
-import UserData from './components/UserData';
 import UserLocation from './components/UserLocation';
 
 function App() {
   const [toggle, setToggle] = useState(false)
   const [previousUsers, setPreviousUsers] = useState([])
+  // const [longitude, setLongitude] = useState(0)
+  // const [latitude, setLatitude] = useState(0)
+    
 
+  //   function getLocation() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(showPosition, showError);
+  //     console.log(latitude, longitude)
+  //   } else {
+  //     alert('SOMETHING WENT HORRIBLY WRONG')
+  //   }
+  // }
+  // getLocation()
+  
+  // function showPosition(position) {
+  //   setLatitude(position.coords.latitude)
+  //   setLongitude(position.coords.longitude)
+  // }
 
+  // function showError(error) {
+  //   switch(error.code) {
+  //     case error.PERMISSION_DENIED:
+  //       console.log("User denied the request for Geolocation.")
+  //       break;
+  //     case error.POSITION_UNAVAILABLE:
+  //       console.log("Location information is unavailable.")
+  //       break;
+  //     case error.TIMEOUT:
+  //       console.log("The request to get user location timed out.")
+  //       break;
+  //     case error.UNKNOWN_ERROR:
+  //       console.log("An unknown error occurred.")
+  //       break;
+  //   }
+  // }
 
   useEffect(() => {
     const getAllUserData = async () => {
@@ -44,7 +76,7 @@ function App() {
         </div>
       </nav>
       <div className='main-container'>
-        <UserData/>
+        {/* <UserData/> */}
       </div>
       </div>
   );
--- client/src/components/UserLocation.jsx
+++ client/src/components/UserLocation.jsx
@@ -4,24 +4,22 @@ import SatelliteData from './SatelliteData';
 export default function UserLocation() {
 const [longitude, setLongitude] = useState(0)
 const [latitude, setLatitude] = useState(0)
- 
   
   //https://developers.google.com/maps/documentation/javascript/geolocation#maps_map_geolocation-javascript
   function getLocation() {
-    // if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    // } else {
-    //   alert('SOMETHING WENT HORRIBLY WRONG')
-    // }
+    if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+      alert('SOMETHING WENT HORRIBLY WRONG')
+    }
   }
-  getLocation()
-  
   function showPosition(position) {
     setLatitude(position.coords.latitude)
     setLongitude(position.coords.longitude)
   }
-  console.log(latitude)
-  console.log(longitude)
+  getLocation()
+
+  console.log(latitude, longitude)
   return (
     <div>
       <SatelliteData latitude={latitude} longitude={longitude} />
